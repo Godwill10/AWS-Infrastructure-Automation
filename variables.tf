cd5dbe6b58f0d7f549b2a4c08a24d2dc52f3bc83
Godwill10 <88179621+Godwill10@users.noreply.github.com>
2026-08-19 11:04:00 -0500

    Secure Terraform database credentials

--- variables.tf
+++ variables.tf
@@ -7,3 +7,15 @@ variable "instance2_name" {
   description = "Name of second EC2 instance"
   type        = string
 }
+
+variable "db_username" {
+  description = "Administrator username for the RDS instance"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "Administrator password for the RDS instance. Supply through TF_VAR_db_password or an untracked .tfvars file."
+  type        = string
+  sensitive   = true
+}
